4d20990d1eb8e87f18cabc55917f959b48e2effc
Tommy <tommy.azzino@gmail.com>
2020-04-24 12:38:44 +0200

    Add DmgTspec creation and function to compute SP duration

--- scratch/evaluate_scheduler_qd_dense_scenario.cc
+++ scratch/evaluate_scheduler_qd_dense_scenario.cc
@@ -121,6 +121,8 @@ PacketCountMap macRxDataOk;
 
 Ptr<DmgApWifiMac> apWifiMac;
 
+Ptr<DmgApWifiMac> apWifiMac;
+
 template <typename T>
 std::string to_string_with_precision (const T a_value, const int n = 6)
 {
