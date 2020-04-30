144b2382d70af8d3d73da61066d21a0fc265b7bb
Tommy <tommy.azzino@gmail.com>
2020-04-30 11:26:39 +0200

    Add traced callback at Mac High for packets forwarded Up
    Add timestamp tag to packets queued at Mac High

--- src/wifi/model/regular-wifi-mac.cc
+++ src/wifi/model/regular-wifi-mac.cc
@@ -798,6 +798,7 @@ void
 RegularWifiMac::ForwardUp (Ptr<Packet> packet, Mac48Address from, Mac48Address to)
 {
   NS_LOG_FUNCTION (this << packet << from << to);
+  m_forwardUpTrace (packet, from, to);
   m_forwardUp (packet, from, to);
 }
 
@@ -1535,6 +1536,10 @@ RegularWifiMac::GetTypeId (void)
                      "The header of unsuccessfully transmitted packet.",
                      MakeTraceSourceAccessor (&RegularWifiMac::m_txErrCallback),
                      "ns3::WifiMacHeader::TracedCallback")
+    .AddTraceSource ("ForwardUp",
+                     "The received MAC packet is forwarded to higher layers",
+                     MakeTraceSourceAccessor (&RegularWifiMac::m_forwardUpTrace),
+                     "ns3::RegularWifiMac::ForwardUpTracedCallback")
   ;
   return tid;
 }
